32b7702ba8abd20dfd2cee0977548afc3f512dd2
Triston Pang <triston@span.io>
2020-09-08 23:12:37 +0800

    Add controller, model, and CRUD API routes

--- api-routes.js
+++ api-routes.js
@@ -1,11 +1,25 @@
 let router = require("express").Router();
+let contactController = require("./contactController");
 
 // Default API response
 router.get("/", function (req, res) {
-    res.json({
-        status: 'API working!',
-        message: 'Welcome to the server'
-    });
+  res.json({
+    status: "API working!",
+    message: "Welcome to the server",
+  });
 });
+
+// Contact routes
+router
+  .route("/contacts")
+  .get(contactController.index)
+  .post(contactController.new);
+
+router
+  .route("/contacts/:contact_id")
+  .get(contactController.view)
+  .patch(contactController.update)
+  .put(contactController.update)
+  .delete(contactController.delete);
 
 module.exports = router;
