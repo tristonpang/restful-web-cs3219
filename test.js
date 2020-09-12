1e4b598a1b0840103452826f220d4ea5ef1e5650
Triston Pang <triston@span.io>
2020-09-13 01:34:00 +0800

    Add Chai

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
 let assert = require('assert');
+const { expect } = require('chai');
 
 describe('Array', () => {
   it('should return -1 when value is not present', () => {
     assert.equal([1,2,3].indexOf(4), -1);
+  });
+  
+  it('should verify testString\'s value and type', () => {
+    var testString = 'foo'
+    expect(testString).to.be.a('string');
+    expect(testString).to.equal('foo');
   });
 });
