ced6574c5b91f78248b97be9638e28cbbb7f1f38
Triston Pang <triston@span.io>
2020-09-15 17:50:22 +0800

    Add API tests

--- test.js
+++ test.js
@@ -1,29 +1,132 @@
-let assert = require("assert");
 const chai = require("chai");
+const like = require("chai-like");
+chai.use(like);
+
 const { expect } = chai;
 const server = require("./index");
 const chaiHttp = require("chai-http");
 
+const Contact = require("./contactModel");
+
 chai.use(chaiHttp);
 
-describe("Array", () => {
-  it("should return -1 when value is not present", () => {
-    assert.equal([1, 2, 3].indexOf(4), -1);
+describe("API", () => {
+  let fetchContactId;
+  const testContact1Json = {
+    name: "John Doe",
+    gender: "Male",
+    email: "test1@gmail.com",
+    phone: "12345678",
+  };
+  const newContact = {
+    name: "Mark John",
+    gender: "Male",
+    email: "newcontact@gmail.com",
+    phone: "11111111",
+  };
+
+  const testContact2Json = {
+    name: "Jeremy Doe",
+    email: "editedcontact@gmail.com",
+  };
+
+  before((done) => {
+    Contact.remove({}, (err) => {
+      done();
+    });
+    var testContact1 = new Contact();
+    testContact1.name = "John Doe";
+    testContact1.gender = "Male";
+    testContact1.email = "test1@gmail.com";
+    testContact1.phone = "12345678";
+
+    testContact1.save();
   });
 
-  it("should verify testString's value and type", () => {
-    var testString = "foo";
-    expect(testString).to.be.a("string");
-    expect(testString).to.equal("foo");
-  });
-
-  it("should successfully GET /test", (done) => {
+  it("should successfully GET all contacts", (done) => {
     chai
       .request(server)
-      .get("/api/test")
+      .get("/api/contacts")
       .end((err, res) => {
-        expect(res.body.message).to.equal("Test success");
+        expect(res.body.data).to.be.like([
+          testContact1Json,
+        ]);
+        fetchContactId = res.body.data[0]["_id"];
         done();
       });
   });
+
+  it("should successfully POST a new contact", (done) => {
+    chai
+      .request(server)
+      .post(`/api/contacts`)
+      .send(newContact)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like(newContact);
+        done();
+      });
+  });
+
+  it("should successfully show all contacts correctly after adding a contact", (done) => {
+    chai
+      .request(server)
+      .get(`/api/contacts`)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like([testContact1Json, newContact]);
+        done();
+      });
+  });
+
+  it("should successfully GET a specific contact", (done) => {
+    chai
+      .request(server)
+      .get(`/api/contacts/${fetchContactId}`)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like(testContact1Json);
+        done();
+      });
+  });
+
+  it("should successfully update a specific contact", (done) => {
+    chai
+      .request(server)
+      .put(`/api/contacts/${fetchContactId}`)
+      .send(testContact2Json)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like(testContact2Json);
+        done();
+      });
+  });
+
+  it("should successfully show all contacts correctly after updating a contact", (done) => {
+    chai
+      .request(server)
+      .get(`/api/contacts`)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like([testContact2Json, newContact]);
+        done();
+      });
+  });
+
+  it("should successfully DELETE a specific contact", (done) => {
+    chai
+      .request(server)
+      .delete(`/api/contacts/${fetchContactId}`)
+      .end((err, res) => {
+        expect(res.body.message).to.be.equal('Contact deleted');
+        done();
+      });
+  });
+
+  it("should successfully show all contacts correctly after deleting a contact", (done) => {
+    chai
+      .request(server)
+      .get(`/api/contacts`)
+      .end((err, res) => {
+        expect(res.body.data).to.be.like([newContact]);
+        done();
+      });
+  });
+
+  
 });
